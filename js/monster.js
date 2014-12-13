31782126d1cfc78457847cc8ea229e1419857bb4
mo khan <mo@mokhan.ca>
2014-12-12 22:13:13 -0700

    move coordinate calculation out of the draw block.

--- js/monster.js
+++ js/monster.js
@@ -7,7 +7,6 @@ function Monster(){
   this.width = this.randomIntFromRange(6, 10);
   this.height = this.randomIntFromRange(6, 10);
   this.speed = this.randomIntFromRange(1, 3);
-  this.rotation = 0;
   this.direction = 0;
   this.sprite = new Image(32, 32);
   this.sprite.src = 'img/enemy.bmp';
@@ -16,22 +15,20 @@ function Monster(){
 }
 
 Monster.prototype.redrawOn = function(world) {
+  if (this.position.y > world.height || this.position.x > world.width) {
+    this.position.y = 0;
+    this.position.x = this.randomIntFromRange(0, world.width);
+    this.velocity.x = 0;
+    this.velocity.y = 0;
+    this.direction = this.toRadians(this.randomIntFromRange(70,90));
+  }
+  this.velocity.x = (this.speed * Math.cos(this.direction));
+  this.velocity.y = (this.speed * Math.sin(this.direction));
+  this.position.x += this.velocity.x;
+  this.position.y += this.velocity.y;
+
   var that = this;
   world.draw(function(view) {
-    if (that.position.y > world.height || that.position.x > world.width) {
-      that.position.y = 0;
-      that.position.x = that.randomIntFromRange(0, world.width);
-      that.velocity.x = 0;
-      that.velocity.y = 0;
-      that.rotation = 0;
-      that.direction = that.toRadians(that.randomIntFromRange(70,90));
-    }
-    that.velocity.x = (that.speed * Math.cos(that.direction));
-    that.velocity.y = (that.speed * Math.sin(that.direction));
-    that.position.x += that.velocity.x;
-    that.position.y += that.velocity.y;
-    that.rotation += 0.01;
-
     view.drawImage(that.sprite, that.position.x, that.position.y);
   });
 };
---
+++ spec/monster_spec.js
@@ -0,0 +1,14 @@
+describe ("Monster", function() {
+  var Monster = require("../js/monster.js");
+  var subject;
+
+  beforeEach(function(){
+    subject = new Monster();
+  });
+
+  describe ("move", function() {
+    it ("moves forward", function() {
+      
+    });
+  });
+});
