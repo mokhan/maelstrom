02a690396707e96017eec889b009edf9d7240b7f
mo khan <mo@mokhan.ca>
2014-12-13 22:34:13 -0700

    add specs for monster.

--- spec/monster_spec.js
+++ spec/monster_spec.js
@@ -1,19 +1,32 @@
 var Monster = require("../js/monster.js");
+
 describe ("Monster", function() {
   var subject;
-  var sprite;
+  var sprite = {};
+  var coordinate = { forward: function(){ }, draw: function(){} };
 
   beforeEach(function(){
-    //subject = new Monster(sprite);
+    subject = new Monster(coordinate, sprite);
   });
 
-  describe("nextMove", function() {
-    describe("when heading north", function(){
-      it ("moves one space up", function() {
-        //subject.position = {x: 100, y: 100};
-        //subject.nextMove(world);
-        //expect(subject.position.x).toEqual(99);
-      });
+  describe("redrawOn", function() {
+    var world = {draw: function(){}};
+
+    beforeEach(function(){
+      spyOn(coordinate, 'forward');
+      spyOn(world, 'draw');
+    });
+
+    it ("moves forward", function() {
+      subject.redrawOn(world);
+
+      expect(coordinate.forward).toHaveBeenCalledWith(world, subject.speed);
+    });
+
+    it ("draws the current position on the canvas", function() {
+      subject.redrawOn(world);
+
+      expect(world.draw).toHaveBeenCalled();
     });
   });
 });
