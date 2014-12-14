8d05e8afd6a42db5267d3d57b630b2e5e3be4c91
mo khan <mo@mokhan.ca>
2014-12-13 21:53:17 -0700

    add specs for rolling around the world.

--- js/coordinate.js
+++ js/coordinate.js
@@ -8,6 +8,19 @@ function Coordinate(options){
 Coordinate.prototype.forward = function(world, speed) {
   this.x += (speed * Math.cos(this.direction));
   this.y += (speed * Math.sin(this.direction));
+
+  if (this.y < 0) {
+    this.y = world.height - this.y - 1;
+  }
+  if (this.x < 0) {
+    this.x = world.width - this.x - 1;
+  }
+  if (this.x > world.width) {
+    this.x = this.x - world.width - 1;
+  }
+  if (this.y > world.height) {
+    this.y = this.y - world.height - 1;
+  }
 };
 
 module.exports = Coordinate;
--- spec/coordinate_spec.js
+++ spec/coordinate_spec.js
@@ -6,7 +6,7 @@ describe("Coordinate", function(){
     var world;
 
     beforeEach(function(){
-      world = {};
+      world = {width: 100, height: 100};
     });
 
     describe ("when heading north", function() {
@@ -19,16 +19,36 @@ describe("Coordinate", function(){
         expect(subject.x).toEqual(100);
         expect(subject.y).toEqual(99);
       });
+
+      it ("rolls around to the other side of the world", function() {
+        subject = new Coordinate({ x: 100, y: 0, direction: (Math.PI / 2) * -1 });
+        speed = 1;
+
+        subject.forward(world, speed);
+
+        expect(subject.x).toEqual(100);
+        expect(subject.y).toEqual(100);
+      });
     });
 
     describe ("when heading east", function() {
       it ("advances forward one space", function() {
+        subject = new Coordinate({x: 50, y: 50, direction: 0});
+        speed = 1;
+
+        subject.forward(world, speed);
+
+        expect(subject.x).toEqual(51);
+        expect(subject.y).toEqual(50);
+      });
+
+      it ("rolls around to the other side of the world", function() {
         subject = new Coordinate({x: 100, y: 100, direction: 0});
         speed = 1;
 
         subject.forward(world, speed);
 
-        expect(subject.x).toEqual(101);
+        expect(subject.x).toEqual(0);
         expect(subject.y).toEqual(100);
       });
     });
@@ -43,17 +63,37 @@ describe("Coordinate", function(){
         expect(subject.x).toEqual(99);
         expect(subject.y).toEqual(100);
       });
-    });
 
-    describe ("when heading south", function() {
-      it ("advances forward one space", function() {
-        subject = new Coordinate({x: 100, y: 100, direction: Math.PI / 2 });
+      it ("rolls around to the other side of the world", function() {
+        subject = new Coordinate({ x: 0, y: 100, direction: Math.PI });
         speed = 1;
 
         subject.forward(world, speed);
 
         expect(subject.x).toEqual(100);
-        expect(subject.y).toEqual(101);
+        expect(subject.y).toEqual(100);
+      });
+    });
+
+    describe ("when heading south", function() {
+      it ("advances forward one space", function() {
+        subject = new Coordinate({x: 100, y: 50, direction: Math.PI / 2 });
+        speed = 1;
+
+        subject.forward(world, speed);
+
+        expect(subject.x).toEqual(100);
+        expect(subject.y).toEqual(51);
+      });
+
+      it ("rolls around to the other side of the world", function() {
+        subject = new Coordinate({ x: 100, y: 100, direction: Math.PI / 2 });
+        speed = 1;
+
+        subject.forward(world, speed);
+
+        expect(subject.x).toEqual(100);
+        expect(subject.y).toEqual(0);
       });
     });
   });
