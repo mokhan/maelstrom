9386237aba10958d43bda8a92ad336317da8c6f2
mo khan <mo@mokhan.ca>
2014-12-23 12:11:49 -0700

    remove the laser when it crashes into a monster.

--- public/javascripts/laser.js
+++ public/javascripts/laser.js
@@ -1,18 +1,29 @@
+var Monster = require('./monster');
+
 function Laser(sprite) {
   _.bindAll(this, 'redrawOn', 'collideWith');
   this.sprite = sprite;
+  this.destroyed = false;
 }
 
 Laser.prototype.redrawOn = function(world) {
-  if (world.inBounds(this.sprite)) {
-    this.sprite = this.sprite.moveForward(world);
-    world.render(this.sprite);
-  } else {
+  if (this.destroyed) {
     world.remove(this);
+  }
+  else {
+    if (world.inBounds(this.sprite)) {
+      this.sprite = this.sprite.moveForward(world);
+      world.render(this.sprite);
+    } else {
+      world.remove(this);
+    }
   }
 };
 
 Laser.prototype.collideWith = function(otherProp) {
+  if (otherProp.constructor === Monster) {
+    this.destroyed = true;
+  }
 };
 
 module.exports = Laser;
