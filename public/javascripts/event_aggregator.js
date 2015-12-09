c71cb546cd8404ce24f877346d106c79d64e2107
mo khan <mo@mokhan.ca>
2015-12-08 21:24:26 -0700

    allow subscribers to subscribe to specific events.

--- public/javascripts/event_aggregator.js
+++ public/javascripts/event_aggregator.js
@@ -2,15 +2,16 @@ var _ = require('underscore');
 
 function EventAggregator(){
   _.bindAll(this, 'subscribe', 'publish');
-  this.subscribers = [];
+  this.subscribers = {};
 };
 
-EventAggregator.prototype.subscribe = function(subscriber) {
-  this.subscribers.push(subscriber);
+EventAggregator.prototype.subscribe = function(event, subscriber) {
+  this.subscribers[event] = this.subscribers[event] || [];
+  this.subscribers[event].push(subscriber);
 }
 
-EventAggregator.prototype.publish = function(message) {
-  _.each(this.subscribers, function(subscriber){
+EventAggregator.prototype.publish = function(event, message) {
+  _.each(this.subscribers[event], function(subscriber){
     subscriber(message);
   });
 }
--- spec/event_aggregator_spec.js
+++ spec/event_aggregator_spec.js
@@ -9,12 +9,22 @@ describe("EventAggregator", function(){
   describe ("publish", function() {
     it ("notifies all subscribers", function() {
       called = false;
-      subject.subscribe(function(message){
+      subject.subscribe('greeting', function(message){
         called = message;
       });
 
-      subject.publish('hello');
+      subject.publish('greeting', 'hello');
       expect(called).toEqual('hello');
+    });
+
+    it ("does not notify subscribers for a different event", function() {
+      called = false;
+      subject.subscribe('other', function(message){
+        called = message;
+      });
+
+      subject.publish('greeting', 'hello');
+      expect(called).toEqual(false);
     });
   });
 })
